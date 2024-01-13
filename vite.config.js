35858ec46f74908a0461f8b208f59f48d0e7199f
Tofan-afk <datspy300@gmail.com>
2024-01-13 10:34:01 +0200

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import mpa from "vite-plugin-multi-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/The-Architects-Website/",
+  rollupOptions: {
+    input: {
+      index: "src/pages/index.html",
+      about: "src/pages/about.html",
+      gallery: "src/pages/gallery.html",
+      certifications: "src/pages/certifications.html",
+      projects: "src/pages/projects.html",
+    },
+  },
   plugins: [
     react(),
     mpa({
